bee44f0d350b142ea257115452e44407a4523a24
NicolaFerracin <nicola.ferracin@gmail.com>
2017-06-14 23:46:36 +0200

    Work on the frontend for adding a transaction

--- public/js/budgetify.js
+++ public/js/budgetify.js
@@ -1,8 +1,8 @@
 import '../sass/style.scss';
 import $ from 'jquery';
-
 window.jQuery = $;
 window.$ = $;
+import '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js';
 
 import scripts from './modules/scripts';
 scripts.init();
