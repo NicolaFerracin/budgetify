81b486980f911daa781679718a143abf24f68bf5
NicolaFerracin <nicola.ferracin@gmail.com>
2017-07-10 21:52:44 +0200

    Add landing page plus about page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('layout');
+    res.render('landing-page');
+});
+
+router.get('/about', (req, res) => {
+    res.render('landing-page-about');
 });
 
 router.get('/favicon.ico', (req, res) => res.sendStatus(204));
